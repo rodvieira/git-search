8569b521845546666b8749d9ba1f3e799b58f660
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-26 23:56:39 -0300

    Criado componente de listagem de usuarios

---
+++ src/components/list-item-user/ListItemUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper-list-item-user">
+    <avatar />
+    <h4>JULIALOPEZ</h4>
+  </div>
+</template>
+<script>
+import Avatar from '@/components/avatar/Avatar.vue';
+
+export default {
+  props: {
+    size: String,
+    bgColor: String,
+  },
+  components: {
+    Avatar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .wrapper-list-item-user {
+    display: flex;
+    align-items: center;
+    border-bottom: 0.5px solid rgba(0, 0, 0, 0.5);
+    padding: 8px 0;
+
+    &:last-child {
+      border: none;
+    }
+
+    img {
+      border-radius: 50%;
+      max-width: 50px;
+    }
+
+    h4 {
+      color: #162668;
+      font-weight: 600;
+      font-size: 15px;
+      margin-left: 16px;
+    }
+  }
+</style>
