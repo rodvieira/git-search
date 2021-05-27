c2986c377542b41ce35ee2ebaa9fa0b895a708d0
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-26 23:54:56 -0300

    Adicionado novas funcionalidade para o componente de Input

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -1,23 +1,45 @@
 <template>
-  <input type="text" :placeholder="placeholder">
+  <div class="wrapper-input">
+    <input type="text" :placeholder="placeholder" :class="{'filter': actionType === 'filter' }">
+  </div>
 </template>
 <script>
 export default {
   props: {
     placeholder: String,
+    actionType: String,
   },
 };
 </script>
 
-<style style="scss">
-  input {
-    border: 1px solid #90A4AE;
-    padding: 4px 6px;
-    border-radius: 4px;
-    height: 30px;
+<style lang="scss" scoped>
+  .wrapper-input {
     width: 100%;
-    font-size: 15px;
-    margin-bottom: 19px;
-    background-color: #fff ;
+
+    input {
+      width: 100%;
+      font-size: 15px;
+      background-color: #fff;
+      font-weight: 600;
+
+      &:not(.filter) {
+        border: 1px solid #90A4AE;
+        padding: 4px 6px;
+        border-radius: 4px;
+        height: 30px;
+        margin-bottom: 18px;
+      }
+
+      &.filter {
+        padding: 8px 16px 8px 40px;
+        height: 35px;
+        box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5);
+        border-radius: 5px;
+        border: none;
+        background-image: url('../../assets/icons/search.svg');
+        background-repeat: no-repeat;
+        background-position: 10px center;
+      }
+    }
   }
 </style>
