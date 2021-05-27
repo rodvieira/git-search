868dec548eb008423d912c9f58329c587cfb6eff
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-26 23:53:04 -0300

    Criado componente de avatar

---
+++ src/components/avatar/Avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="@/assets/icons/user-avatar.svg" alt="Avatar">
+</template>
+<script>
+export default {
+  props: {
+    size: String,
+    bgColor: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  img {
+    border-radius: 50%;
+    max-width: 50px;
+  }
+</style>
