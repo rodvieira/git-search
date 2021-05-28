d1f60c2cca1fb403a5057c57a89e3c4be85c0623
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-27 22:36:58 -0300

    Criado componente de detalhes

---
+++ src/components/details/Details.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper-details">
+    <img :src="require(`@/assets/icons/${icon}`)" alt="Icone detalhe" />
+    <span>
+      {{quantity}}
+    </span>
+    <h6>{{title}}</h6>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    icon: String,
+    title: String,
+    quantity: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .wrapper-details {
+    color: #162668;
+    font-weight: 600;
+    display: inline-block;
+
+    img {
+      display: inline-block;
+      vertical-align: middle;
+      margin-right: 4px;
+    }
+
+    span {
+      font-size: 14px;
+    }
+
+    h6 {
+      font-size: 14px;
+      line-height: 20px;
+    }
+
+  }
+</style>
