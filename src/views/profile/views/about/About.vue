1a5e1ecb697c1ffc7978b08b97ae02af70524c12
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-30 14:01:01 -0300

    Criado views de projetos e sobre

---
+++ src/views/profile/views/about/About.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper-about">
+    <span class="item-title">Bio</span>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Nulla laoreet tincidunt erat ut dapibus.
+      Sed vitae ante et dolor euismod fermentum eu vitae nunc.
+      Sed eget massa ut eros condimentum tincidunt id quis enim.
+      Suspendisse ipsum lectus, ullamcorper et fermentum id, sodales id elit.
+      Nam ligula purus, volutpat at pellentesque vel,
+      congue non purus. Phasellus quis ex dapibus,
+      elementum ipsum ut, scelerisque elit. Duis eget metus eu enim hendrerit euismod.
+      Quisque tortor odio, molestie ut hendrerit non, faucibus quis tortor.
+    </p>
+    <div class="item-infos">
+      <img src="@/assets/icons/home.svg" alt="Home">
+      <span class="item-title">San Francisco</span>
+    </div>
+    <div class="item-infos">
+      <img src="@/assets/icons/computer.svg" alt="Computer">
+      <span class="item-title">https://fulano.com</span>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+  .wrapper-about {
+    text-align: left;
+
+    .item-title {
+      color: #162668;
+      font-size: 16px;
+      font-weight: 600;
+
+      &:first-child {
+        font-size: 18px;
+      }
+    }
+
+    p {
+      font-family: Montserrat;
+      font-size: 16px;
+      line-height: 22px;
+      margin: 14px 0;
+    }
+
+    .item-infos {
+      display: flex;
+      align-items: center;
+      margin-bottom: 14px;
+
+      img {
+        margin-right: 14px;
+      }
+    }
+  }
+</style>
---
+++ src/views/profile/views/projects/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper-projects">
+    <item-project />
+  </div>
+</template>
+<script>
+import ItemProject from '@/components/item-project/ItemProject.vue';
+
+export default {
+  components: {
+    ItemProject,
+  },
+};
+</script>
