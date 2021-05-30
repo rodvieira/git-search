fc39fceddbd7d83e8fb9bb2ba1ecb98c151d721e
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-30 16:26:43 -0300

    Implementação das regras para view de listagem

--- src/views/list-users/ListUsers.vue
+++ src/views/list-users/ListUsers.vue
@@ -1,23 +1,89 @@
 <template>
   <div class="wrapper-list-users">
-    <input-filter action-type="filter" />
+    <input-filter action-type="filter" @input-value="e => setQueryFilter(e)" />
     <div class="list-users">
-      <list-item-user @click="$router.push({ path: 'Profile'})"/>
-      <list-item-user />
-      <list-item-user />
-      <list-item-user />
+      <item-user
+        v-for="user in users"
+        :key="user.id"
+        :infoUser="user"
+        @click="$router.push({ path: 'Profile'})"
+      />
+      <div class="status-list">
+        <loader v-if="loading"/>
+        <view-more v-else-if="totalList > users.length" @click="viewMoreList()"/>
+        <itens-loaded
+          v-else
+          :itensLoaded="users.length"
+          listName="usuários"
+        />
+      </div>
     </div>
   </div>
 </template>
 <script>
 import InputFilter from '@/components/input/Input.vue';
-import ListItemUser from '@/components/list-item-user/ListItemUser.vue';
+import ItemUser from '@/components/item-user/ItemUser.vue';
+import ItensLoaded from '@/components/itens-loaded/ItensLoaded.vue';
+import Loader from '@/components/loader/Loader.vue';
+import ViewMore from '@/components/view-more/ViewMore.vue';
+import { mapState } from 'vuex';
+
+import searchApi from '@/service/routes/search';
 
 export default {
   name: 'ListUsers',
   components: {
     InputFilter,
-    ListItemUser,
+    ItemUser,
+    Loader,
+    ViewMore,
+    ItensLoaded,
+  },
+  data() {
+    return {
+      users: [],
+      loading: true,
+      totalList: 0,
+      filter: '',
+      page: 1,
+    };
+  },
+  computed: {
+    ...mapState(['searchParams']),
+  },
+  methods: {
+    searchUsers(query) {
+      this.setSearchParams(query);
+      this.$router.push({ path: '/list' });
+    },
+    viewMoreList() {
+      const query = this.filter ? this.filter : this.searchParams;
+      this.page += 1;
+      this.fetchUsers(query);
+    },
+    setQueryFilter(query) {
+      this.filter = query;
+      this.page = 1;
+      this.fetchUsers(query);
+    },
+    async fetchUsers(query) {
+      this.loading = true;
+      try {
+        const { data } = await searchApi.search({
+          q: query,
+          per_page: 10 * this.page,
+        });
+        this.users = data.items;
+        this.totalList = data.total_count;
+      } catch (err) {
+        console.log(err);
+        this.users = [];
+      }
+      this.loading = false;
+    },
+  },
+  mounted() {
+    this.fetchUsers(this.searchParams);
   },
 
 };
@@ -31,5 +97,8 @@ export default {
       margin-top: 24px;
     }
 
+    .status-list {
+      margin: 24px;
+    }
   }
 </style>
