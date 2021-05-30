5e0502765935d0cecafd800870ca9add9d5d2719
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-30 16:26:16 -0300

    Criado componentes auxiliares para as listagem

---
+++ src/components/itens-loaded/ItensLoaded.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    {{
+      itensLoaded > 0 ? `Todos os ${listName} carregados` : `Nenhum resultado encontrado`
+    }}
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    itensLoaded: Number,
+    listName: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  span {
+    padding: 4px 8px;
+    color: #162668;
+    font-weight: bold;
+  }
+</style>
---
+++ src/components/view-more/ViewMore.vue
@@ -0,0 +1,14 @@
+<template>
+  <a class="wrapper-view-more">
+    Ver mais
+  </a>
+</template>
+
+<style lang="scss" scoped>
+  .wrapper-view-more {
+    margin-top: 16px;
+    padding: 4px 8px;
+    color: #162668;
+    font-weight: bold;
+  }
+</style>
