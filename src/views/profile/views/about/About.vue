7f2762cde81415a476c49a5e7c670c0f5a729794
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-05-30 19:03:27 -0300

    Implementado tab sobre

--- src/views/profile/views/about/About.vue
+++ src/views/profile/views/about/About.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="wrapper-about">
     <span class="item-title">Bio</span>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Nulla laoreet tincidunt erat ut dapibus.
-      Sed vitae ante et dolor euismod fermentum eu vitae nunc.
-      Sed eget massa ut eros condimentum tincidunt id quis enim.
-      Suspendisse ipsum lectus, ullamcorper et fermentum id, sodales id elit.
-      Nam ligula purus, volutpat at pellentesque vel,
-      congue non purus. Phasellus quis ex dapibus,
-      elementum ipsum ut, scelerisque elit. Duis eget metus eu enim hendrerit euismod.
-      Quisque tortor odio, molestie ut hendrerit non, faucibus quis tortor.
-    </p>
+    <p>{{user.bio}}</p>
     <div class="item-infos">
       <img src="@/assets/icons/home.svg" alt="Home">
-      <span class="item-title">San Francisco</span>
+      <span class="item-title">{{user.location}}</span>
     </div>
     <div class="item-infos">
       <img src="@/assets/icons/computer.svg" alt="Computer">
-      <span class="item-title">https://fulano.com</span>
+      <span class="item-title">{{user.company}}</span>
     </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 
 export default {
+  computed: {
+    ...mapState(['user']),
+  },
 };
 </script>
 
@@ -36,6 +30,7 @@ export default {
       color: #162668;
       font-size: 16px;
       font-weight: 600;
+      word-break: break-all;
 
       &:first-child {
         font-size: 18px;
